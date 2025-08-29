616e5d425e19450ca854dac2c04828bb4e73238b
JoeGuillory <joeguillory15@gmail.com>
2025-08-29 16:58:22 -0500

    Added LightAnimatorComponent

---
+++ Source/CGGame/LightAnimatorComponent.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "LightAnimatorComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CGGAME_API ULightAnimatorComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	ULightAnimatorComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void FlickerLight();
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	FString LightBlinkingPattern;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	float FlickerSpeed;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<class ULightComponentBase> LightComponent;
+		
+};
--- Source/CGGame/ProceduralCubeActor.cpp
+++ Source/CGGame/ProceduralCubeActor.cpp
@@ -19,14 +19,14 @@ AProceduralCubeActor::AProceduralCubeActor()
 void AProceduralCubeActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	DrawDebugPoint(GetWorld(), Positions[0]+ RootComponent->GetRelativeLocation(), 10.0f, FColor::Blue);
+	DrawDebugPoint(GetWorld(), Positions[0]+ GetActorLocation(), 10.0f, FColor::Blue);
 	DrawDebugPoint(GetWorld(), Positions[1]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Green);
 	DrawDebugPoint(GetWorld(), Positions[2]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Yellow);
-	DrawDebugPoint(GetWorld(), Positions[3]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Red);
-	DrawDebugPoint(GetWorld(), Positions[4]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Blue);
-	DrawDebugPoint(GetWorld(), Positions[5]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Green);
-	DrawDebugPoint(GetWorld(), Positions[6]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Yellow);
-	DrawDebugPoint(GetWorld(), Positions[7]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Red);
+	//DrawDebugPoint(GetWorld(), Positions[3]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Red);
+	//DrawDebugPoint(GetWorld(), Positions[4]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Blue);
+	//DrawDebugPoint(GetWorld(), Positions[5]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Green);
+	//DrawDebugPoint(GetWorld(), Positions[6]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Yellow);
+	//DrawDebugPoint(GetWorld(), Positions[7]+ RootComponent->GetRelativeLocation(),10.0f, FColor::Red);
 }
 
 // Called when the game starts or when spawned
@@ -38,13 +38,16 @@ void AProceduralCubeActor::BeginPlay()
 	Positions.Add(FVector(0, -50, -50)); // 2nd vert
 	Positions.Add(FVector(0, -50, 50)); // 3rd vert
 	Positions.Add(FVector(0, 50, -50));
-	Positions.Add(FVector(-100, 50, 50));
+	/*Positions.Add(FVector(-100, 50, 50));
 	Positions.Add(FVector(-100, -50,-50));
 	Positions.Add(FVector(-100, -50,50));
-	Positions.Add(FVector(-100, 50,-50));
-	UVs.Add({ 1,1 });
+	Positions.Add(FVector(-100, 50,-50));*/
+
 	UVs.Add({ 0,0 });
+	UVs.Add({ 1,1 });
 	UVs.Add({ 1,0 });
+	UVs.Add({ 0,1 });
+
 	TriangleIndices.Add(0);
 	TriangleIndices.Add(1);
 	TriangleIndices.Add(2);
@@ -53,45 +56,45 @@ void AProceduralCubeActor::BeginPlay()
 	TriangleIndices.Add(1);
 	TriangleIndices.Add(0);
 
-	TriangleIndices.Add(4);
-	TriangleIndices.Add(3);
-	TriangleIndices.Add(0);
+	//TriangleIndices.Add(4);
+	//TriangleIndices.Add(3);
+	//TriangleIndices.Add(0);
 
-	TriangleIndices.Add(7);
-	TriangleIndices.Add(3);
-	TriangleIndices.Add(4);
+	//TriangleIndices.Add(7);
+	//TriangleIndices.Add(3);
+	//TriangleIndices.Add(4);
 
-	TriangleIndices.Add(2);
-	TriangleIndices.Add(5);
-	TriangleIndices.Add(6);
+	//TriangleIndices.Add(2);
+	//TriangleIndices.Add(5);
+	//TriangleIndices.Add(6);
 
-	TriangleIndices.Add(1);
-	TriangleIndices.Add(5);
-	TriangleIndices.Add(2);
+	//TriangleIndices.Add(1);
+	//TriangleIndices.Add(5);
+	//TriangleIndices.Add(2);
 
-	TriangleIndices.Add(4);
-	TriangleIndices.Add(2);
-	TriangleIndices.Add(6);
+	//TriangleIndices.Add(4);
+	//TriangleIndices.Add(2);
+	//TriangleIndices.Add(6);
 
-	TriangleIndices.Add(0);
-	TriangleIndices.Add(2);
-	TriangleIndices.Add(4);
+	//TriangleIndices.Add(0);
+	//TriangleIndices.Add(2);
+	//TriangleIndices.Add(4);
 
-	TriangleIndices.Add(6);
-	TriangleIndices.Add(7);
-	TriangleIndices.Add(4);
+	//TriangleIndices.Add(6);
+	//TriangleIndices.Add(7);
+	//TriangleIndices.Add(4);
 
-	TriangleIndices.Add(5);
-	TriangleIndices.Add(7);
-	TriangleIndices.Add(6);
-	//Bottom
-	TriangleIndices.Add(5);
-	TriangleIndices.Add(3);
-	TriangleIndices.Add(7);
+	//TriangleIndices.Add(5);
+	//TriangleIndices.Add(7);
+	//TriangleIndices.Add(6);
+	////Bottom
+	//TriangleIndices.Add(5);
+	//TriangleIndices.Add(3);
+	//TriangleIndices.Add(7);
 
-	TriangleIndices.Add(1);
-	TriangleIndices.Add(3);
-	TriangleIndices.Add(5);
+	//TriangleIndices.Add(1);
+	//TriangleIndices.Add(3);
+	//TriangleIndices.Add(5);
 	ProceduralMesh->CreateMeshSection(0, Positions, TriangleIndices, TArray<FVector>(), UVs, TArray<FColor>(), TArray<FProcMeshTangent>(), true);
 	ProceduralMesh->CastShadow = false;
 }
